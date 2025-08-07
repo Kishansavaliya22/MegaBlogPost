70bc3d759e6259b3f019d49ab1eb44ccfcc65668
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-08-07 20:11:43 +0530

    Envronment Variable declaration amd AppWrite Setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APPWRITE_URL: string;
+  readonly VITE_APPWRITE_DATABASE_ID: string;
+  readonly VITE_APPWRITE_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_BUCKET_ID: string;
+  readonly VITE_APPWRITE_PROJECT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
