23e7be34c2cccb732fb2f88b8d6768f45e9843c5
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-08-12 12:40:27 +0530

    Basic App component layout design and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import appwriteauth from "./appwrite/appWriteAuth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+// import { Outlet } from "react-router-dom";
 
-function App() {
-  return (
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    appwriteauth.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login(userData));
+      } else {
+        dispatch(logout());
+      }
+
+      setLoading(false);
+    });
+  }, []);
+
+  return !loading ? (
     <>
-      <h1>A Blogging Application with Appwrite</h1>
+      <Header />
+      <main>TODO {/* <Outlet />  */}</main>
+      <Footer />
     </>
-  );
-}
+  ) : null;
+};
 
 export default App;
