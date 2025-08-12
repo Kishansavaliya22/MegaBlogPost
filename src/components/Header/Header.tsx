2329111868c51d05f8e4751fa27361fb9353512f
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-08-12 23:17:33 +0530

    Header redesign and logo component making

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import type { Rootstate } from "../../store/store";
+import { NavLink } from "react-router";
 
 const Header = () => {
-  return <div>Header</div>;
+  const authStatus = useSelector((state: Rootstate) => state.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      location: "/",
+      loginstatus: authStatus,
+    },
+    {
+      name: "Login",
+      location: "/login",
+      loginstatus: authStatus,
+    },
+    {
+      name: "SignUp",
+      location: "/signup",
+      loginstatus: authStatus,
+    },
+    {
+      name: "BlogList",
+      location: "/bloglist",
+      loginstatus: !authStatus,
+    },
+    {
+      name: "post",
+      location: "/post",
+      loginstatus: !authStatus,
+    },
+  ];
+
+  return (
+    <header>
+      <nav>
+        <div>
+          <NavLink to="/">
+            <Logo />
+          </NavLink>
+        </div>
+        <div></div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 
 const store = configureStore({
-  reducer: { authReducer },
+  reducer: authReducer,
 });
 
 export default store;
+
+export type Rootstate = ReturnType<typeof store.getState>;
